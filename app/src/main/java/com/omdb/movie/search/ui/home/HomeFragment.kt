d4ec12d970551c336420c0107acde433b335417c
naderahmedwashere <naderwashereagain@gmail.com>
2024-02-07 16:09:06 +0530

    implemented BaseFragment on HomeFragment, MovieDetailsFragment and SearchResultsFragment

--- app/src/main/java/com/omdb/movie/search/ui/home/HomeFragment.kt
+++ app/src/main/java/com/omdb/movie/search/ui/home/HomeFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import com.omdb.movie.search.R
+import com.omdb.movie.search.base.BaseFragment
+import com.omdb.movie.search.databinding.FragmentHomeBinding
+import com.omdb.movie.search.ui.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 // TODO: Rename parameter arguments, choose names that match
@@ -14,6 +18,11 @@ private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
 @AndroidEntryPoint
-class HomeFragment : Fragment() {
+class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel, MainViewModel>(
+    R.layout.fragment_home,
+    HomeViewModel::class
+) {
+    val activityViewModel: MainViewModel by activityViewModels()
+    override fun activityViewModel(): MainViewModel = activityViewModel
 
 }
--- app/src/main/java/com/omdb/movie/search/ui/moviedetails/MovieDetailsFragment.kt
+++ app/src/main/java/com/omdb/movie/search/ui/moviedetails/MovieDetailsFragment.kt
@@ -5,20 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import com.omdb.movie.search.R
+import com.omdb.movie.search.base.BaseFragment
+import com.omdb.movie.search.databinding.FragmentMovieDetailsBinding
+import com.omdb.movie.search.ui.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MovieDetailsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 @AndroidEntryPoint
-class MovieDetailsFragment : Fragment() {
-
+class MovieDetailsFragment : BaseFragment<FragmentMovieDetailsBinding,MovieDetailsViewModel,MainViewModel>(R.layout.fragment_movie_details,MovieDetailsViewModel::class) {
+    val activityViewModel : MainViewModel by activityViewModels()
+    override fun activityViewModel(): MainViewModel = activityViewModel
 }
